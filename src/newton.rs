4d78e1f430ca3676f0798e85dd4a921d2143d3bf
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2023-11-03 08:47:50 +0100

    add main parts

--- src/newton.rs
+++ src/newton.rs
@@ -4,7 +4,23 @@ use std::time::Instant;
 
 use super::status::{Status, StatusCode};
 
-pub struct Params {}
+pub struct Params {
+    pub tol: f64,
+    pub max_steps: usize,
+    pub verbose: usize,
+    pub time_limit: f64,
+}
+
+impl Params {
+    pub fn new() -> Self {
+        Params {
+            tol: 1e-4,
+            max_steps: usize::MAX,
+            verbose: 0,
+            time_limit: f64::INFINITY,
+        }
+    }
+}
 
 pub fn solve(
     problem: &dyn Problem,
@@ -15,5 +31,49 @@ pub fn solve(
     let start = Instant::now();
     let n = problem.size();
     let mut status = Status::new(n);
+    let mut step: usize = 0;
+    let mut stop = false;
+    loop {
+        status.steps = step;
+        let elapsed = start.elapsed().as_secs_f64();
+        status.time = elapsed;
+        if step >= params.max_steps {
+            status.code = StatusCode::MaxSteps;
+            stop = true;
+        }
+        if params.time_limit > 0.0 && elapsed >= params.time_limit {
+            status.code = StatusCode::TimeLimit;
+            stop = true;
+        }
+
+        if let Some(callback_fn) = callback {
+            stop = callback_fn(&status);
+            if stop {
+                status.code = StatusCode::Callback;
+            }
+        };
+
+        let optimal = problem.is_optimal(&status, params.tol);
+
+        if params.verbose > 0 && (step % params.verbose == 0 || optimal) {
+            println!(
+                "{:10} {:10.2} {:10.6} {:10.6} {:8.3}",
+                step, elapsed, status.violation, status.value, status.asum,
+            )
+        }
+
+        if optimal {
+            status.code = StatusCode::Optimal;
+            stop = true;
+        }
+
+        if stop {
+            break;
+        }
+
+        // TODO: implement something
+
+        step += 1;
+    }
     status
 }
